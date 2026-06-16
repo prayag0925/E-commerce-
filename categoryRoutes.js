56405218635a0769b7f40f9ceb8bad9c823fe094
prayag0925 <prayag0925@gmail.com>
2026-06-16 12:18:32 +0530

    Update categoryRoutes.js

--- categoryRoutes.js
+++ categoryRoutes.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const { verifyToken, isAdmin } = require('../middleware/authMiddleware');
 
-// All category routes require a logged-in user
+
 router.use(verifyToken);
 
-// View all categories
 router.get('/', categoryController.getAllCategories);
 
-// Add, edit, delete categories - admin only
+
 router.post('/add', isAdmin, categoryController.addCategory);
 router.post('/edit/:id', isAdmin, categoryController.updateCategory);
 router.get('/delete/:id', isAdmin, categoryController.deleteCategory);
