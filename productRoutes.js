2054b9c1ec7349c4fecadacb5b5a5f52b6bcf592
prayag0925 <prayag0925@gmail.com>
2026-06-16 12:15:43 +0530

    Update productRoutes.js

--- productRoutes.js
+++ productRoutes.js
@@ -3,25 +3,25 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
-// All product routes require a logged-in user
+
 router.use(verifyToken);
 
-// Product listing routes
+
 router.get('/', productController.getAllProducts);
 router.get('/my-products', productController.getMyProducts);
 
-// Add product
+
 router.get('/add', productController.getAddProductForm);
 router.post('/add', productController.postAddProduct);
 
-// Edit product
+
 router.get('/edit/:id', productController.getEditProductForm);
 router.post('/edit/:id', productController.postUpdateProduct);
 
-// Delete product
+
 router.get('/delete/:id', productController.deleteProduct);
 
-// Single product detail (keep this route last so it doesn't catch other routes)
+
 router.get('/:id', productController.getProductItem);
 
 module.exports = router;
